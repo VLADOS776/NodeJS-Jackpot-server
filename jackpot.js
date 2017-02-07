c90bcd6a1c964ca6ed81a7904778ffb294d7550b
VLADOS776 <kurtukovvlad@gmail.com>
2017-02-07 23:30:33 +0300

    Access-Control-Allow-Credentials

--- jackpot.js
+++ jackpot.js
@@ -20,6 +20,7 @@ app.use((req,res) =>  {
     if (allowed.test(origin))
         res.setHeader("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.end('Jackpot server for Open Case Simulator');
 })
 
